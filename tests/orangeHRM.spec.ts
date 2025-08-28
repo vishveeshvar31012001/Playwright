06f30044e0cfd4bc1fdaa0156365bb73614f92b7
vishveeshvar31012001 <vishveeshvar31012001@gmail.com>
2025-08-28 12:35:48 +0530

    Update orangeHRM.spec.ts

--- tests/orangeHRM.spec.ts
+++ tests/orangeHRM.spec.ts
@@ -1,8 +1,8 @@
-import {expect, test} from '@playwright/test'
-test('First demo Test',async({page})=>{
+import {expect, test} from '@playwright/test';
+test("Validate Orange HRM Login Screen",async({page})=>{
     await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
     await page.getByRole('textbox',{name:'username'}).fill('Admin')
     await page.getByRole('textbox',{name:'password'}).fill('admin123')
-    await page.getByRole('button',{name:'Login'}).click();
-   
+    await page.getByRole('button',{name:'Login'}).click()
+    await expect(page).toHaveTitle("OrangeHRM")
 })
